727f7b4e4639116799d1ded93c5b382e090aa211
kalyani <dharitha04@gmail.com>
2018-03-02 11:07:07 -0600

    first program

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[3];
+
+            numbers[0] = 286;
+            numbers[1] = 287;
+            numbers[2] = 288;
+
+            foreach (int k in numbers)
+            {
+                Console.WriteLine(k);
+                Console.ReadLine();
+            }
+        }
+    }
+}
